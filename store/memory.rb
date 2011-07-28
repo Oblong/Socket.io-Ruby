4c5c3e5abc0450a923dbb776b4e26639756e7625
Chris McKenzie <chris@oblong.com>
2011-07-27 18:21:28 -0700

    +memory

---
+++ store/memory.rb
@@ -0,0 +1,48 @@
+class Memory < Store
+  def initialize opts
+    super(opts)
+  end
+
+  def publish; end
+  def subscribe; end
+  def unsubscribe; end
+end
+
+class Client < Store
+  def initialize(*arguments)
+    super(arguments)
+    @data = {}
+  end
+
+  def get key, fn
+    fn(nil, @data[key])
+    self
+  end
+
+  def set(key, value, fn=nil)
+    @data[key] = value
+    fn
+    self
+  end
+
+  def has key, fn
+    fn(nil, @data[key])
+  end
+
+  def del key, fn
+    @data.delete key
+    fn
+    self
+  end
+
+  def destroy(expiration=nil)
+    if expiration.nil?
+      @data = {}
+    else
+      #TODO
+      @closeTimeout = EventMachine::Timer.new(Manager.settings['close timeout'] * 1000) do | x |
+        @data = {}
+      end
+    end
+  end
+end
