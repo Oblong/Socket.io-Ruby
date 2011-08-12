332b0b8dfe0c741dede4aad55384e2b911d6e241
Chris McKenzie <chris@oblong.com>
2011-08-11 17:37:14 -0700

    Rewrote the autoloader

--- socket.io.rb
+++ socket.io.rb
@@ -1,14 +1,21 @@
-$base = File.dirname(__FILE__)
-
-$:.unshift $base, $base + '/store/'
-
 require 'rubygems'
 require 'rack'
 require 'json'
 require 'uuid'
 require 'cgi'
 
+module SocketIO
+  ROOT = File.expand_path(File.dirname(__FILE__))
 
-require 'FileSession'
-require 'ipc'
-require 'manager'
+  autoload :Logger, "#{ROOT}/logger"
+  autoload :Manager, "#{ROOT}/manager"
+  autoload :SocketNamespace, "#{ROOT}/namespace"
+  autoload :Manager, "#{ROOT}/manager"
+  autoload :Parser, "#{ROOT}/parser"
+  
+  autoload :Store, "#{ROOT}/store/store"
+  autoload :FileSession, "#{ROOT}/store/FileSession"
+  autoload :Memory, "#{ROOT}/store/memory"
+
+  require "#{ROOT}/trasport"
+end
